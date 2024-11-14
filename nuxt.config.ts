9f94cb62776eba5293151a5db78dbc9877459820
Rashidik Sadykov <rashidiksadykov@gmail.com>
2024-11-15 04:12:31 +0500

    TG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    telegramBotToken: process.env.TELEGRAM_BOT_TOKEN
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
+
+ 
+});
